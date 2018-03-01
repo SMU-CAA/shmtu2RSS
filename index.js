49b3b24f2689c6a92504058598145a423a7c4d0d
Simon Shi <simonsmh@gmail.com>
2018-03-01 08:47:59 +0800

    Add support for unix socket

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var logger = require('./tools/logger');
+var fs = require('fs');
 
 logger.info(`🍻 shmtu2RSS start! Cheers!`);
 
@@ -7,4 +8,13 @@ var app = express();
 app.all('*', require('./routes/all'));
 app.get('/events', require('./routes/events'));
 app.get('/notes', require('./routes/notes'));
-app.listen(1203);
+
+var socket = '/var/run/shmtu2RSS.socket'
+if(fs.existsSync(socket)){
+fs.unlinkSync(socket);
+}
+app.listen(socket);
+process.on('SIGINT', () => {
+fs.unlinkSync(socket);
+process.exit();
+});
