8b34801ee35a68b82c777c4115267e125ddc22ec
Simon Shi <simonsmh@gmail.com>
2018-04-29 19:00:35 +0800

    Fix location

--- tools/logger.js
+++ tools/logger.js
@@ -1,9 +1,10 @@
 var log4js = require('log4js');
+var path = require('path');
 log4js.configure({
     appenders: {
         shmtu2RSS: {
             type: 'file',
-            filename: 'logs/shmtu2RSS.log',
+            filename: path.resolve(__dirname, '../logs/shmtu2RSS.log'),
             maxLogSize: 2048,
             backups: 3,
             compress: true
